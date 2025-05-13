a00785da9eeb4ed5eed383cac7b807f5f83bcdf5
APCSAwork <dannyt26@nycstudents.net>
2025-05-13 09:37:41 -0400

    Update PartB.java

--- Q1/PartB.java
+++ Q1/PartB.java
@@ -1,1 +1,11 @@
-
+public int dogWalkShift(int startHour, int end){
+        int total = 0;
+        for(int hour = startHour; hour <= endHour; hour++){
+            int dogs = walkDogs(hour);
+            total += dogs*5;
+            if(dogs == maxDogs || (hour >= 9 && hour <= 17)){
+                total +=3;
+            }
+        }
+        return total;
+    }
