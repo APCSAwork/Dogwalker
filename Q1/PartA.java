0943dabadf1608bbe1221bfb3bc00a781e620c7d
APCSAwork <dannyt26@nycstudents.net>
2025-05-13 09:37:18 -0400

    Update PartA.java

--- Q1/PartA.java
+++ Q1/PartA.java
@@ -1,1 +1,13 @@
+public int walkDogs(int hour){
+        int numDogs = numAvaiableDogs(hour);
+        if(maxDogs > numDogs){
+            updateDogs(hour, numDogs);
+            return numDogs;
+            
+        }
+        else{
+            updateDogs(hour, naxDogs);
+            return maxDogs;
 
+        }
+    }
