34951fd80bf08f5cc49c7a131cc268baf1a6951c
APCSAwork <dannyt26@nycstudents.net>
2025-05-15 10:54:33 -0400

    Update gamaA.java

--- Q4/gamaA.java
+++ Q4/gamaA.java
@@ -1,1 +1,11 @@
+public SumOrSameGame(int numRow, int numCols){
+        int[][] game = new int[numRow][numCols];
+        for(int i = 0 ; i < game.length ; i++){
+            for(int j = 0 ; j < game[0].length ; j++){
+                game[i][j] = (int)(Math.random()* 9)+1;
+                
+            }
 
+        }
+
+    }
