388b608120c395ebb5c98bb94b6da1df9b2fe7fc
APCSAwork <dannyt26@nycstudents.net>
2025-05-15 11:04:12 -0400

    Update gameB.java

--- Q4/gameB.java
+++ Q4/gameB.java
@@ -1,1 +1,20 @@
+public boolean clearPoint(int numRow, int numCols){
+        boolean check = true;
+        int object = puzzle[numRow][numCols];
+        for(int i = numRow ; i < game.length ; i++){
+            for(int j = 0 ; j < game[0].length ; j++){
+                if(check && j == numCols){
+                    check = false;
+                }
+                if(puzzle[i][j]+ object == 10 || object == puzzle[i][j]){
+                    puzzle[numRow][numCols] = 0;
+                    puzzle[i][j] = 0;
+                    return true;
+                }
+            }
 
+        }
+        return false;
+            
+
+    }
